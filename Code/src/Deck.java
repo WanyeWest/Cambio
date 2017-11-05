f08e679d7df022c0b7cd54aa04a3976770eccc51
Wayne Flores <waynef711@gmail.com>
2017-11-04 20:07:25 -0800

    Made first methods in Deck class
    also started doing things to the Game class

--- Code/src/Deck.java
+++ Code/src/Deck.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Integer> cards;
+
+    public Deck(ArrayList<Integer> cards) {
+        this.cards = cards;
+    }
+
+    public void addCard(int cardVal) {
+        cards.add(cardVal);
+    }
+
+    public void addCard(int cardVal, int cardIndex) {
+        cards.add(cardIndex, cardVal);
+    }
 }
--- Code/src/Game.java
+++ Code/src/Game.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
+import static java.util.Collections.shuffle;
+
 public class Game {
+    public static void main(String[] args) {
+        /*
+        Array of unshuffled deck
+        -1 is red king
+        11 is jack
+        12 is queen
+        13 is black king
+         */
+        int[] fullDeck = {
+                -1,-1,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,
+                6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,
+                12,12,12,12,13,13
+        };
+        ArrayList<Integer> deck = new ArrayList<Integer>();
+        for(int i: fullDeck) {
+            deck.add(i);
+        }
+        shuffle(deck);
+
+        Deck gameDeck = new Deck(deck);
+    }
 }
